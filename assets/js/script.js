f6b7b5bf3147b6c1b1551b3d050f9ced157533c2
cjsolis <cjelizondo12@gmail.com>
2023-03-23 20:27:59 -0500

    changed api call to stay in the same category user chose

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,21 @@ var questionBox = document.querySelector(".questions")
 var answerBox = document.querySelector(".answers")
 var progressBar = document.querySelector('.progress');
 let questionIndex = 0
-
+var score = 0
+var categoryChoice 
 
 // API call to trivia categories
-var getQuestionCategory = function(categoryChoice) {
-    fetch(`https://the-trivia-api.com/api/questions?categories=${categoryChoice}&limit=1`)
+var getQuestionCategory = function(categoryChoice, questionIndex) {
+    fetch(`https://the-trivia-api.com/api/questions?categories=${categoryChoice}`)
         .then((res)=>{
             return res.json();
     })
         .then((responseData)=>{
-            let currentQuestion = responseData[0].question
+            console.log(responseData)
+            let currentQuestion = responseData[questionIndex].question
+            console.log(currentQuestion)
             questionBox.innerHTML= currentQuestion
-            console.log(responseData) 
-            createAnswers(responseData)       
+            createAnswers(responseData[questionIndex])          
     })
     .catch(error => console.log(error));
 }
@@ -26,9 +28,9 @@ var getQuestionCategory = function(categoryChoice) {
 // getting the value of the button clicked
 function categoryButtonClicked(){
    startTimer()
-   var categoryChoice = this.value
+   categoryChoice = this.value
    console.log(categoryChoice)
-   getQuestionCategory(categoryChoice)
+   getQuestionCategory(categoryChoice, questionIndex)
 
    main.style.display = 'none';
    brainHandEl.style.display= 'none';
@@ -65,13 +67,13 @@ function startTimer(){
 }
 
 // Creating answer choices array from api answers and displaying
-var createAnswers = function(responseData){
+var createAnswers = function(questionData){
     $('#answers').empty()
     let answersArr= [];
 
-    answersArr.push(responseData[0].correctAnswer)
+    answersArr.push(questionData.correctAnswer)
     for (let i =0; i < 3; i++){
-        answersArr.push(responseData[0].incorrectAnswers[i])
+        answersArr.push(questionData.incorrectAnswers[i])
     }
 
     let shuffledArray = answersArr.sort((a,b) => 0.5 - Math.random())
@@ -86,25 +88,26 @@ var createAnswers = function(responseData){
         answerBox.append(answerBtns)
         answerBtns.addEventListener('click', function(event){
             answerClicked(event)
-            getQuestionCategory() //this fires next question but the questions are from other categories 
         });
     }
 
     function answerClicked(event) {
         console.log("answer clicked")
         const selectedAnswer = event.target.innerHTML
-        if(selectedAnswer === responseData[0].correctAnswer){
+        if(selectedAnswer === questionData.correctAnswer){
             // function for tracking points
-            console.log("correct!")
+            score ++
+            console.log(`correct! ${score}`)
         } else{
             console.log("incorrect")
         }
         // need to stop the timer or else it will keep
         // going and think an answer wasnt selected
         clearInterval(timeInterval);
+        questionIndex++;
+        getQuestionCategory(categoryChoice, questionIndex)
     };
 }
 
 
-// Start button event listener 
 
